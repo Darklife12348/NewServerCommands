ded290e5e8df97e21d0e418e07d947ab0d63b708
Darklife12345 <86660324+Darklife12348@users.noreply.github.com>
2022-07-11 21:05:09 -0500

    Adds Intellij minecraft plugin template

---
+++ src/main/java/me/darklife12345/nsc/NSC.java
@@ -0,0 +1,17 @@
+package me.darklife12345.nsc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NSC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
